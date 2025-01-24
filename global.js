7c1a85f9d9fe1034e5338f3b3e4c6b46a65e3578
nadang18 <nathandang1802@gmail.com>
2025-01-24 10:35:50 -0800

    update js
    js code to make color selector work

--- global.js
+++ global.js
@@ -57,4 +57,10 @@ document.body.insertAdjacentHTML(
     </label>`
 );
 
+const select = document.querySelector('#color-scheme-switch');
+
+select.addEventListener('input', function (event) {
+    console.log('color scheme changed to', event.target.value);
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
+  });
   
