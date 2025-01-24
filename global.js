09798bf7d536814acde71e2df3e86321af972cee
nadang18 <nathandang1802@gmail.com>
2025-01-24 01:41:59 -0800

    edit js
    step 3

--- global.js
+++ global.js
@@ -3,3 +3,44 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// const navLinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
+
+// currentLink?.classList.add('current');
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'https://github.com/nadang18', title: 'GitHub Profile' }
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname
+      );
+
+    a.toggleAttribute('target', a.host !== location.host);
+    
+    nav.append(a);
+  }
+  
