ef2ca1681cbc5fd6449eb06adc92c37c7c7cb410
nadang18 <nathandang1802@gmail.com>
2025-01-30 19:11:38 -0800

    add projects to home

--- projects/projects.js
+++ projects/projects.js
@@ -1,7 +1,8 @@
 import { fetchJSON, renderProjects } from '../global.js';
+
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
-console.log(projects);
+
 
 document.querySelector('.header').textContent = projects.length + ' Projects';
