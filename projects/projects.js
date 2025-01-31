cee4e2ac16d2f906810df178e55e9369381c5dec
nadang18 <nathandang1802@gmail.com>
2025-01-30 19:20:48 -0800

    update home page

--- projects/projects.js
+++ projects/projects.js
@@ -4,5 +4,5 @@ const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 
-
+// Project count on the header
 document.querySelector('.header').textContent = projects.length + ' Projects';
