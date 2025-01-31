0149884eddfd4bcf5e9824ba74f948f1babdf5fb
nadang18 <nathandang1802@gmail.com>
2025-01-30 17:18:33 -0800

    update projects
    update dynamic project using js

--- global.js
+++ global.js
@@ -94,3 +94,40 @@ select.addEventListener('input', function (event) {
       location.href = url;
   });
   
+export async function fetchJSON(url) {
+  try {
+      // Fetch the JSON file from the given URL
+      const response = await fetch(url);
+
+      // Check if the fetch request was successful
+      if (!response.ok) {
+          throw new Error(`Failed to fetch projects: ${response.statusText}`);
+      }
+
+      // Parse the response into JSON format
+      const data = await response.json();
+      return data;
+
+  } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+    // Clear existing content
+    containerElement.innerHTML = '';
+
+    for (let proj in project) {
+      // Create an article element for the project
+      const article = document.createElement('article');
+
+      article.innerHTML = `
+          <${headingLevel}>${proj.title}</${headingLevel}>
+          <img src="${proj.image}" alt="${proj.title}">
+          <p>${proj.description}</p>
+      `;
+
+      // Append the article to the container element
+      containerElement.appendChild(article);
+    }
+}
