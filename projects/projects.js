71230f5c24e6783d2950eeb07511427695d1ed1a
nadang18 <nathandang1802@gmail.com>
2025-01-30 17:43:24 -0800

    update header for projects

--- projects/projects.js
+++ projects/projects.js
@@ -3,3 +3,5 @@ const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 console.log(projects);
+
+document.querySelector('.header').textContent = projects.length + ' Projects';
