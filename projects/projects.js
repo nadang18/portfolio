2957e9b703d22f9ec5ebc308fc598c7bf526d88b
nadang18 <nathandang1802@gmail.com>
2025-02-08 20:24:16 -0800

    add pie chart

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
@@ -6,3 +7,39 @@ renderProjects(projects, projectsContainer, 'h2');
 
 // Project count on the header
 document.querySelector('.header').textContent = projects.length + ' Projects';
+
+
+// D3.js
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+// let arc = arcGenerator({
+//     startAngle: 0,
+//     endAngle: 2 * Math.PI,
+//   });
+// d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+
+
+// Pie chart
+let data = [1, 2];
+let total = 0;
+let colors = ['gold', 'purple'];
+
+for (let d of data) {
+  total += d;
+}
+let angle = 0;
+let arcData = [];
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+
+let arcs = arcData.map((d) => arcGenerator(d));
+arcs.forEach((arc, idx) => {
+    d3.select('svg')
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', colors[idx])
+})
+
