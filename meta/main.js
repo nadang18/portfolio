520f114df5bff493442eafd8ee3a6644aeb4ec18
nadang18 <nathandang1802@gmail.com>
2025-02-14 10:45:23 -0800

    step 1

--- meta/main.js
+++ meta/main.js
@@ -1,4 +1,5 @@
 let data = [];
+let commits = [];
 
 async function loadData() {
   data = await d3.csv('loc.csv', (row) => ({
@@ -9,6 +10,38 @@ async function loadData() {
     date: new Date(row.date + 'T00:00' + row.timezone),
     datetime: new Date(row.datetime),
   }));
+
+  processCommits();
+  console.log(commits);
+}
+
+function processCommits() {
+  commits = d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+      let { author, date, time, timezone, datetime } = first;
+      let ret = {
+        id: commit,
+        url: 'https://github.com/YOUR_REPO/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
+
+      Object.defineProperty(ret, 'lines', {
+        value: lines,
+        configurable: true,
+        writable: true,
+        enumerable: false,
+      });
+
+      return ret;
+    });
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
