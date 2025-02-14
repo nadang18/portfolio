819d866ca2df400336754a5ed3ab3854fde2c250
nadang18 <nathandang1802@gmail.com>
2025-02-14 13:31:17 -0800

    update stats

--- meta/main.js
+++ meta/main.js
@@ -1,73 +1,74 @@
 let data = [];
-let commits = [];
-let xScale, yScale;  // Make scales global
-let rScale;
-let brushSelection = null; 
-let dots;
+
+async function loadData() {
+  data = await d3.csv('loc.csv');
+  console.log(data);
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  await loadData();
+});
 
 async function loadData() {
     data = await d3.csv('loc.csv', (row) => ({
-        ...row,
-        line: Number(row.line),
-        depth: Number(row.depth),
-        length: Number(row.length),
-        date: new Date(row.date + 'T00:00' + row.timezone),
-        datetime: new Date(row.datetime),
+      ...row,
+      line: Number(row.line), // or just +row.line
+      depth: Number(row.depth),
+      length: Number(row.length),
+      date: new Date(row.date + 'T00:00' + row.timezone),
+      datetime: new Date(row.datetime),
     }));
+  }
 
-
-document.addEventListener('DOMContentLoaded', async () => {
-    await loadData();
-  });
-
-    processCommits();
-    displayStats();
-    createScatterplot();
-}
+  let commits = d3.groups(data, (d) => d.commit);
 
 function processCommits() {
-    commits = d3.groups(data, (d) => d.commit)
-        .map(([commit, lines]) => {
-            let first = lines[0];
-            let { author, date, time, timezone, datetime } = first;
-            let ret = {
-                id: commit,
-                url: `https://github.com/vis-society/lab-7/commit/${commit}`,
-                author,
-                date,
-                time,
-                timezone,
-                datetime,
-                hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
-                totalLines: lines.length,
-            };
+  commits = d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+      let { author, date, time, timezone, datetime } = first;
+      let ret = {
+        id: commit,
+        url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
 
-            Object.defineProperty(ret, 'lines', {
-                value: lines,
-                writable: false,
-                enumerable: false,
-                configurable: false
-            });
+      Object.defineProperty(ret, 'lines', {
+        value: lines,
+        // What other options do we need to set?
+        // Hint: look up configurable, writable, and enumerable
+      });
 
-            return ret;
-        });
+      return ret;
+    });
 }
+
+async function loadData() {
+    // original function as before
+  
+    processCommits();
+    console.log(commits);
+  }
 
 function displayStats() {
-  const dl = d3.select("#stats").append("dl").attr("class", "stats");
+  // Process commits first
+  processCommits();
 
-  dl.append("dt").html("Total <abbr title='Lines of code'>LOC</abbr>");
-  dl.append("dd").text(data.length);
+  // Create the dl element
+  const dl = d3.select('#stats').append('dl').attr('class', 'stats');
 
-  dl.append("dt").text("Total Commits");
-  dl.append("dd").text(commits.length);
+  // Add total LOC
+  dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+  dl.append('dd').text(data.length);
 
-  const numFiles = d3.groups(data, (d) => d.file).length;
-  dl.append("dt").text("Number of Files");
-  dl.append("dd").text(numFiles);
-
-  const avgLineLength = d3.mean(data, (d) => d.length);
-  dl.append("dt").text("Average Line Length (chars)");
-  dl.append("dd").text(avgLineLength.toFixed(2));
+  // Add total commits
+  dl.append('dt').text('Total commits');
+  dl.append('dd').text(commits.length);
 }
-
